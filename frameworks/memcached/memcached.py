89304cb8650efbe394791c9e39ae09a5ecfdeee6
Daniele Venzano <venza@brownhat.org>
2016-07-19 15:36:38 +0200

    Add a memcached framework

---
+++ frameworks/memcached/memcached.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2016, Daniele Venzano
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+def memcached_service():
+    """
+    :rtype: dict
+    """
+    service = {
+        'name': "memcached",
+        'docker_image': 'memcached',
+        'monitor': True,
+        'required_resources': {"memory": 64 * 1024 * 1024},  # 64M
+        'ports': [],
+        'environment': [],
+        'volumes': [],
+        'command': '',
+        'total_count': 1,
+        'essential_count': 1,
+        'startup_order': 0
+    }
+
+    return service
+
