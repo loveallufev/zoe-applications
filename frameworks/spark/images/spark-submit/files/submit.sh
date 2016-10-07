6b8f6354be6903c509f5af4d964063ae14ceefc6
Daniele Venzano <venza@brownhat.org>
2016-10-07 13:20:40 +0200

    Update Spark to 1.6.2 and add HDFS configuration to the submit image

--- frameworks/spark/images/spark-submit/files/submit.sh
+++ frameworks/spark/images/spark-submit/files/submit.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-cd $WS_DIR
+cat /opt/spark-defaults.conf | sed -e "s/XXX_DRIVER_MEMORY/$SPARK_DRIVER_RAM/" > ${SPARK_HOME}/conf/spark-defaults.conf
+cat /opt/core-site.xml | sed -e "s/XXX_NAMENODE_HOST/$NAMENODE_HOST/" > ${HADOOP_HOME}/etc/hadoop/core-site.xml
+cp /opt/hdfs-site.xml ${HADOOP_HOME}/etc/hadoop/
+
+cd $ZOE_WORKSPACE
 
 exec /opt/spark/bin/spark-submit --master spark://${SPARK_MASTER_IP}:7077 --executor-memory=${SPARK_EXECUTOR_RAM} "$@"
 
