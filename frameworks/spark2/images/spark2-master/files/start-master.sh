211c67f3b2a49507a6bf33e75eeb25873610f1eb
Daniele Venzano <venza@brownhat.org>
2016-12-12 16:01:13 +0100

    Upate spark2 images

--- frameworks/spark2/images/spark2-master/files/start-master.sh
+++ frameworks/spark2/images/spark2-master/files/start-master.sh
@@ -3,6 +3,6 @@ if [ -z ${SPARK_MASTER_IP} ]; then
 	export SPARK_MASTER_IP=`awk 'NR==1 {print $1}' /etc/hosts`
 fi
 
-cd $SPARK_HOME
+cd /opt/spark
 ./bin/spark-class org.apache.spark.deploy.master.Master --host $SPARK_MASTER_IP --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT $@
 
--- frameworks/spark2/images/spark2-worker/files/start-worker.sh
+++ frameworks/spark2/images/spark2-worker/files/start-worker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd $SPARK_HOME
+cd /opt/spark
 
 ./bin/spark-class org.apache.spark.deploy.worker.Worker \
 	spark://${SPARK_MASTER_IP}:7077 --cores ${SPARK_WORKER_CORES:-4} --memory ${SPARK_WORKER_RAM:-4g} \
